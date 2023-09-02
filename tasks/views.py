004d499acfdf4882f79aa7f65b50cb76bab0ce4e
Erick <123erickrh@gmail.com>
2023-09-01 23:36:07 -0500

    Create sign up form

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def index(request):
+  return render(request, 'signup.html', {
+    'form': UserCreationForm
+  })
+
 def hello(request):
-  return HttpResponse('Hola')
+  return HttpResponse('Hello world')
