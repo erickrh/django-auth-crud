6f0cf5f13b350cc71bf03dcd59577dc8b570ccf5
Erick <123erickrh@gmail.com>
2023-10-01 22:53:57 -0500

    Add validations. Change sign up and UI. Add widgets on forms

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class Task_form(ModelForm):
+class Task_form(forms.ModelForm):
   class Meta:
     model = Task
     fields = ['title', 'description', 'important']
+    widgets = {
+      'title': forms.TextInput(attrs={'class': 'form--title', 'placeholder': 'Write a title'}),
+      'description': forms.Textarea(attrs={'class': 'form--description', 'placeholder': 'Write a description'}),
+      'important': forms.CheckboxInput(attrs={'class': 'form--important'}),
+    }
